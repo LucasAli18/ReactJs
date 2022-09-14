e46b756710b98741bd9e2e0e554e3bb06356493b
Lucas Ali <lucas.10.ali@gmail.com>
2022-09-14 19:38:50 -0300

    Primera Entrega Final

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,17 +5,17 @@ import "./ItemDetail.css";
 
 
 
-const ItemDetail= ({id,nombre,imagen,precio,descripcion}) =>{
+const ItemDetail= ({id,nombre,imagen,precio,descripcion,stock}) =>{
     return (
                 
-    <div className="row detail  justify-content-center">
+    <div className="row detail  justify-content-center m-5">
         <img src={imagen} alt={nombre} className="col-5"/>
         <div key={id} className="col-5">
             <h3>Six Pack de {nombre}</h3>
             <h4>${precio}</h4>
             <p><span className="material-symbols-outlined">credit_card</span> <b>24</b> Cuotas de <b></b></p>
             <p>{descripcion}</p>
-            <ItemCount/>
+            <ItemCount stock={stock} initial={1} onAdd={0}/>
         </div>
         
 
