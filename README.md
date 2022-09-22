c698ab630611b9d68b7b6daa218bb1a9c8d13348
Lucas Ali <lucas.10.ali@gmail.com>
2022-09-21 21:11:05 -0300

    Desafio Context

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-Cerveceria THE NORTHMAN
-Primer entrega final:
-añadi dos categorias a los productos, promociones y cervezas, para poder cumplir con la consigna y aplicar url dinamicas utilizando react-router-dom.
-Tambien en cada producto, clikeando en el boton "Ver Mas" o en la imagen, te lleva a su ItemDetail correspondiente, en el logo clickeando te lleva ItemListContainer y en la navbar linkee cada li con su respectivo destino.
+Desafio Context : Cree mi componente Context.jsx donde estan las funciones que uso de forma global. Reemplaze el addItem por onAdd, ademas de agregar la funcion removeItem y clear, que de momento estan en el itemCount y el CardWidget respectivamente hasta que cree el componente que muestre el cart.
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -7,18 +7,16 @@ import { CartContext } from "../Context/Context";
 
 const CartWidget = () =>{
     
-    const {cart} = useContext(CartContext);
+    const {cart,clear} = useContext(CartContext);
     const [total, setTotal ] = useState(0);
-    console.log(cart);
     useEffect(()=>{
         let totalItems = cart.reduce((total , item) => total += item.cantidad, 0);
         setTotal(totalItems);
-        console.log(totalItems);
     },[cart] );
 
 
     return (
-        <button className="btn btn-warning position-relative" type="button">
+        <button className="btn btn-warning position-relative" type="button" onClick={()=>clear()}>
             <span className="material-symbols-outlined">shopping_cart</span>
             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{total}</span>
         </button>
