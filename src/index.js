12151607338d6ac087c34f03a2e579c811835d1a
Lucas Ali <lucas.10.ali@gmail.com>
2022-09-28 19:11:04 -0300

    Desafio ItemCollection

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWZ_KtP0si3Gnd5W4XdOBCjXiQ0XA3REk",
+  authDomain: "craftbeer-ae7a4.firebaseapp.com",
+  projectId: "craftbeer-ae7a4",
+  storageBucket: "craftbeer-ae7a4.appspot.com",
+  messagingSenderId: "873465670843",
+  appId: "1:873465670843:web:242ae3f84fd98090c4cd9e"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
